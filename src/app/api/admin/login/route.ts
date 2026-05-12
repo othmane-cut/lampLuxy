e4c63567dea228f8d72bfb682869674694c47dba
othmane <lheothmane@gmail.com>
2026-05-12 23:32:04 +0100

    fix api error for deployment

--- src/app/api/admin/login/route.ts
+++ src/app/api/admin/login/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 import { login } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcryptjs";
